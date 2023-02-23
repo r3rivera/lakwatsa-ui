230976fb2f6348789f4b1f45ebe76c78f58800b7
r3rivera <r.ryan.rivera@outlook.com>
2023-02-22 21:33:24 -0600

    Added initial display of the index.html, issue on the path of JS and CSS

--- src/server/controller/server.controller.ts
+++ src/server/controller/server.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { All, Controller, Get, Header, Req, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 
 @Controller()
 export class ServerController {
   constructor() {}
 
-  @Get()
-  getHello(): string {
-    return "Hello World";
+  @All('*')
+  @Header('Content-Type', 'text/html')
+  root(@Req() request:{}, @Res() response: Response): void {
+    console.info("### Serving static content! ###");
+    response.send(
+      readFileSync(
+        join('dist','ui-resources','index.html'),
+        {
+          encoding: 'utf8'
+        }
+      )
+    );
   }
+
+
+
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { AddressInfo } from 'net';
 import { ServerModule } from './server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ServerModule);
+
+
   await app.listen(3000);
+  const httpServer = await app.getHttpServer();
+  const serverAddress = (await httpServer.address()) as AddressInfo;
+  console.info(
+      `NestJS Server is listening on ${serverAddress.address}:${serverAddress.port}`
+  );
+  return httpServer;
 }
 
 
