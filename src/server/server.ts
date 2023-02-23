9aabf87278bb0b25e402d886e4e6c7fce9b499b7
r3rivera <r.ryan.rivera@outlook.com>
2023-02-22 22:18:30 -0600

    Added bread-compressor, expressStaticGzip libs and fix MIME type error for finding the static resources

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import * as expressStaticGzip from 'express-static-gzip';
 import { AddressInfo } from 'net';
+import { join } from 'path';
 import { ServerModule } from './server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ServerModule);
 
+  /**
+   * Add Middleware Items
+   */
+
+  const staticPath = join(__dirname, '../', 'ui-resources');
+  console.log(`Root Directory Name :: ${__dirname}`);
+  console.log(`Updated Static Directory Name :: ${staticPath}`);
+
+  app.use('/', expressStaticGzip(staticPath,{
+    enableBrotli: true,
+    orderPreference: ['br'],
+    index: false,
+    serveStatic:{
+      maxAge: '365d',
+      etag: true,
+      setHeaders: (res) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+      }
+    }
+  }));
 
   await app.listen(3000);
   const httpServer = await app.getHttpServer();
