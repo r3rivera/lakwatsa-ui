6937203244efc3fb38591da40bfcddfaab42317a
r3rivera <r.ryan.rivera@outlook.com>
2023-02-25 14:13:46 -0600

    Added tailwind typography and line clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.neutral
+      },
+      fontFamily: {
+        // to change, update font in _document.js
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        stock: [defaultTheme.fontFamily.sans]
+      },
+      aspectRatio: {
+        "4/3": "4 / 3",
+        "3/2": "3 / 2",
+        "2/3": "2 / 3",
+        "9/16": "9 / 16"
+      }
+    }
   },
-  plugins: [],
-}
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography")
+  ]
+};
