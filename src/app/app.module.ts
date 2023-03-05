804be1f8482df8f03e10fa11ea62fc51647e4ab6
r3rivera <r.ryan.rivera@outlook.com>
2023-03-04 22:17:48 -0600

    Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './features/login/login.module';
-import { HeaderModule } from './components/header/header.module';
-import { NonSecuredHeaderComponent } from './components/header/non-secured/non-secured-header.component';
-import { SecuredHeaderComponent } from './components/header/secured/secured-header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecuredHeaderComponent,
-    NonSecuredHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    LoginModule,
-    HeaderModule
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
