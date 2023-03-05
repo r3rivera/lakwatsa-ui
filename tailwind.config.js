19e33822c2d5cccaa017dd868aa0b470182a40d0
r3rivera <r.ryan.rivera@outlook.com>
2023-03-04 23:10:21 -0600

    Added daisyui-2.15.13 plugin for tailwind style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,27 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+            "primary": "#D1C1D7",
+            "secondary": "#F6CBD1",
+            "accent": "#B4E9D6",
+            "neutral": "#70ACC7",
+            "base-100": "#FFFFFF",
+            "info": "#3ABFF8",
+            "success": "#36D399",
+            "warning": "#FBBD23",
+            "error": "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/typography"),
-    require("@tailwindcss/forms")
+    require("@tailwindcss/forms"),
+    require("daisyui")
   ]
 };
