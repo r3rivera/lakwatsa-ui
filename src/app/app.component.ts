220772face08fb5cd27711b903955d3e8fc4cf12
r3rivera <r.ryan.rivera@outlook.com>
2023-03-05 22:36:36 -0600

    Added route to login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'r2r-root',
-  template:`<router-outlet></router-outlet>`
+  template:`
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {}
